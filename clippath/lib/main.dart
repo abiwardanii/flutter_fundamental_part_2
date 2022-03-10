2a9f420592eda022aab9827fe80fa3663cdd7546
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-03-10 20:15:40 +0700

    clippath

---
+++ clippath/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('clipper'),
+      ),
+      body: Center(
+        child: ClipPath(
+          clipper: MyClipper(),
+          child: Image(
+            width: 300,
+            image: NetworkImage(
+                "https://scarletviolet.pokemon.com/assets/img/global/screens/5-2x.jpg"),
+          ),
+        ),
+      ),
+    ));
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 2, size.height * 0.75, size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
