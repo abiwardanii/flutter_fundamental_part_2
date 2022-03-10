ba085c7bd0ca2ad93fdc4c45a0802ea010549169
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-03-10 20:16:35 +0700

    gradient_opacity

---
+++ gradient_opacity/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("belajar shademask"),
+        ),
+        body: Center(
+          child: ShaderMask(
+            shaderCallback: (rectangle) {
+              return LinearGradient(
+                      colors: [Colors.black, Colors.transparent],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)
+                  .createShader(
+                      Rect.fromLTRB(0, 0, rectangle.width, rectangle.height));
+            },
+            blendMode: BlendMode.dstIn,
+            child: Image(
+              width: 300,
+              image: NetworkImage(
+                  "https://assets.pokemon.com/assets/cms2/img/video-games/_tiles/pokemon-unite/02112022/pokemon-unite-169.jpg"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
